4b186706733442dc5dc77c8e820df32365f77065
Ben Matthews <ben.matthews@ed.ac.uk>
2020-12-10 21:30:46 +0000

    looking at attendance

--- scripts/modelling.R
+++ scripts/modelling.R
@@ -153,8 +153,96 @@ preds_long %>%
   #filter(within_5k_buffer == 1) %>% 
   ggplot(aes(x = draws, y = Decision, fill = Decision)) +
   ggridges::geom_density_ridges(alpha = 0.3,
-                                stat = "binline") +
-  facet_wrap(~ within_5k_buffer, scales = "free_x")
+                                stat = "binline",
+                                binwidth = 1) +
+  facet_wrap(~ within_5k_buffer, scales = "free_x") +
+  theme_minimal()
+
+
+# looking at attendance on crime (for days when there are games on --------
+
+
+games_only <- 
+  crime_buffer %>% 
+  filter(Decision != "No game")
+
+games_only <- 
+  games_only %>% 
+  mutate(attendance_k = attendance / 1000,
+         attendance_k_s = as.vector(scale(attendance_k, scale = FALSE)))
+
+game_mod <- 
+  mgcv::gam(
+    crimes ~ 1 + Decision * within_5k_buffer + s(doy) 
+    + dow + attendance_k_s + attendance_k_s : within_5k_buffer,
+    offset = log(area),
+    data = games_only,
+    family = "quasipoisson"
+  )
+
+summary(game_mod)
+
+coef(game_mod) %>% 
+  as.data.frame() %>% 
+  exp()
+
+# extra 1k people associated with reduction of 0.7%... but consistent
+# within buffer or not?
+
+new_df_g <- 
+  tibble(
+    Decision = "W",
+    within_5k_buffer = c(1, 0),
+    doy = 175,
+    attendance_k_s = list(c(-5, 0, 5)),
+    dow = 1,
+    area = 5
+  )
+
+new_df_g <- unnest(new_df_g)
+
+
+preds_g <- 
+  broom::augment(
+    game_mod,
+    newdata = new_df_g
+  )
+
+res_g <- summary(game_mod)
+disp_g <- as.data.frame(res_g$dispersion)[[1]]
+
+
+# simulate some expected values incorporating esstimation uncertainty
+# (I think this should probably be mvnorm from the model's vcov, but this
+# will do for now)
+
+preds_long_g <- 
+  preds_g %>% 
+  mutate(est = map2(.fitted, .se.fit, ~ rnorm(500, .x, .y))) %>% 
+  unnest()
+
+# simulate data from these predictions
+
+draws_df <- 
+  preds_long_g %>% 
+  mutate(draws = map(est, ~ rqpois(500, exp(.x), disp_g))) %>% 
+  unnest(draws)
+
+draws_df %>% 
+  #filter(within_5k_buffer == 1) %>% 
+  ggplot(aes(x = draws, y = factor(attendance_k_s), fill = Decision)) +
+  ggridges::geom_density_ridges(alpha = 0.3,
+                                stat = "binline",
+                                binwidth = 1) +
+  facet_wrap(~ within_5k_buffer, scales = "free_x") +
+  labs(
+    y = "Attendance (relative to average attendance)",
+    x = "Simulated crime counts (per square km)",
+    caption = "1 = within 5km of stadium, 0 = elsewhere in LA",
+    title = "Every extra 1k in attendance is associated with ~0.7% less crime",
+    subtitle = "But that's basically too small a difference to see!"
+  ) +
+  theme_minimal()
 
 
 
